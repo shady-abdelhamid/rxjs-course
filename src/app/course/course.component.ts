78cc6bd095cfb79e852b9395576b2fc00c610dd1
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-01-11 16:33:10 +0200

    add course and lessons observable

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,7 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
-
+import { createHttpObservable } from '../common/util';
 
 @Component({
     selector: 'course',
@@ -24,7 +24,9 @@ import {Lesson} from '../model/lesson';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
+    course$ : Observable<Course>;
 
+    lessons$ : Observable<Lesson[]>;
 
     @ViewChild('searchInput', { static: true }) input: ElementRef;
 
@@ -37,7 +39,10 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         const courseId = this.route.snapshot.params['id'];
 
+        this.course$ = createHttpObservable(`/api/courses/${courseId}`);
 
+        this.lessons$ = createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+            .pipe(map(res => Object.values(res['payload'])));
 
     }
 
